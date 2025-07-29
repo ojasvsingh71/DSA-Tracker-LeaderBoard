11cda654fccab23251f846d4c94245809eca1ad6
ojasvsingh71 <ojasvsingh0@gmail.com>
2025-07-29 17:50:35 +0530

    fix

--- backend/services/codeforces.service.js
+++ backend/services/codeforces.service.js
@@ -1,11 +1,12 @@
 import axios from "axios";
+import sanitizeRating from "../utils/sanitizerating.js";
 
 const fetchCodeforcesStats = async (username) => {
     try {
         const res = await axios.get(`https://cp-rating-api.vercel.app/codeforces/${username}`);
 
         return {
-            contestRating: res.data.rating
+            contestRating: sanitizeRating(res.data.rating)
         }
     } catch (err) {
         console.log("Error in fetching codeforces data : -\n");
